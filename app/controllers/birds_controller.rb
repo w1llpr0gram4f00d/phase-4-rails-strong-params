95e47ae363efd512019c87a97b22db7a0927a573
Marc Cavazos <marcavazos19@icloud.com>
2022-03-14 19:17:33 -0500

    commit tree f62a8702d0f1abc8bd90f13ad401904603726c29

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,18 +8,25 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(params.permit(:name, :species))
     render json: bird, status: :created
   end
 
   # GET /birds/:id
   def show
-    bird = Bird.find_by(id: params[:id])
+    bird = Bird.find_by(bird_params)
     if bird
       render json: bird
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  
+  private 
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
